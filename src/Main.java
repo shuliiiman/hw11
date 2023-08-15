4381915dcce5a96275a4f79129f5db82eff190eb
Alexander Shulimov <a.shulimov@yandex.ru>
2023-08-16 00:17:00 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,55 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        checkYear(2021);
+        os(0,2015);
+        delivery(95);
     }
-}
+    public static void checkYear(int year) {
+
+        System.out.println("Задание номер 1");
+        if (year % 4 == 0 && year % 400 == 0 || year % 100 != 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+        }
+        public static void os(int clientOS, int clientDeviceYear ){
+            System.out.println("Задание номер 2");
+            if (clientOS==1 && clientDeviceYear<=2015){
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+            else if(clientOS==1 && clientDeviceYear>2015)  {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+            else if(clientOS!=1 && clientDeviceYear<=2015){
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+            else if(clientOS!=1 && clientDeviceYear>2015) {
+
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+
+            }
+        }
+        public static int delivery(int deliveryDistance){
+            System.out.println("Задание номер 4");
+            int day=1;
+            if (deliveryDistance<20){
+                System.out.println("Потребуется дней: "+ day);
+                return day;
+            }
+            else if(deliveryDistance >=20 && deliveryDistance<60){
+                System.out.println("Потребуется дней: "+ (day+1));
+                return day;
+
+            }
+            else if(deliveryDistance >=60 && deliveryDistance<100){
+                System.out.println("Потребуется дней: "+ (day+2));
+                return day;
+            }
+            else{
+                System.out.println("Доставки нет");
+                return day;
+            }
+        }
+    }
